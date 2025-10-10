5a1e62741216b74165f13fa6f5ba7a7028ab6917
bjeames <bjeames@usfca.edu>
2025-10-10 14:14:58 -0700

    message

--- components/econ_demo.py
+++ components/econ_demo.py
@@ -92,7 +92,6 @@ High-dimensional regression is central in many fields:
             html.P('Finance: forecasting outcomes like unemployment with many indicators.'),
             html.P('Image and speech recognition: working with pixel-level data where p >> n.'),
             html.P('Recommendation systems: filtering huge user–item datasets.'),
-            html.P('Recommendation systems: filtering huge user–item datasets.'),
             html.P('In all of these areas, being able to identify the few predictors that matter is essential. This is why Lasso regression has become a cornerstone of high-dimensional analysis. It connects classical regression ideas to the demands of today’s big data world'),
             html.H3("Econometrics Demo", style={"textAlign": "center", "marginBottom": "0.4rem"}),
             html.P(
